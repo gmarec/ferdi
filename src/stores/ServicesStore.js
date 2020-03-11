c40eaee688b2142e81bf3abe36cae7ad98a2037f
gmarec <gilles.marec@gmail.com>
2020-03-11 17:23:33 +0100

    Revert "Resync fork"

--- src/stores/ServicesStore.js
+++ src/stores/ServicesStore.js
@@ -699,7 +699,6 @@ export default class ServicesStore extends Store {
     const service = this.active;
     if (service) {
       this.actions.service.focusService({ serviceId: service.id });
-      document.title = `Ferdi - ${service.name}`;
     }
   }
 
